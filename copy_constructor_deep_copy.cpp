d5e9c1a84f32a57b7f9d291a0deb10de5e0a9f48
rhemesh <hemeshr07@gmail.com>
2024-11-12 13:24:05 +0530

    user defined copy-->deep copy constructor

---
+++ copy_constructor_deep_copy.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+class character 
+{
+    char fc,lc;
+    public:
+    character(char a,char b)
+    {
+        fc = a;
+        lc = b;
+    }
+    character(character &obj)
+    {
+        cout<<"Inside userdefined copy function"<<endl;
+        fc = obj.fc;
+        lc = obj.lc;
+        cout<<"characters copied"<<endl;
+    }
+
+    void display()
+    {
+        cout<<"First character  = "<<fc<<endl;
+        cout<<"Last character  = "<<lc<<endl;
+    }
+};
+
+int main()
+{
+    character c1('H','E');
+    c1.display();
+    character c2(c1);
+    c2.display();
+
+    return 0;
+
+}
