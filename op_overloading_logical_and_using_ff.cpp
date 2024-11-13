2d5b258ada69ba8af3309cf3a8141081fee1b90e
rhemesh <hemeshr07@gmail.com>
2024-11-13 15:07:01 +0530

    Overloading && operator using friend function

---
+++ op_overloading_logical_and_using_ff.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+class op
+{
+    int x,y;
+    public:
+    op(int a = 0,int b =0) : x(a),y(b) {}
+
+    void print()
+    {
+        cout<<"x = "<<x<<" "<<"y = "<<y<<endl;
+    }
+
+    friend op operator&&(op &s1,op &s2)
+    {
+        op s3;
+        s3.x = s1.x && s2.x;
+        s3.y = s1.y && s2.y;
+        return s3;
+    }
+
+};
+
+int main()
+{
+    op o1(5,3);
+    op o2(45,0);
+    o1.print();
+    o2.print();
+
+    op o3 = o1 && o2;
+    cout<<"After doing && operation values of a and b are:"<<"\n";
+    o3.print();
+
+    return 0;
+
+}
