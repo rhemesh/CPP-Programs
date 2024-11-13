8a6de71090dc8d7ffae41c5203e4ec948d419ee9
rhemesh <hemeshr07@gmail.com>
2024-11-13 08:15:48 +0530

    creating object in friend class instead of creating it in main then we no need to pass the friend func arg

---
+++ creating_obj_in_friend_func.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+
+using namespace std;
+
+class frnd
+{
+    char ch;
+    string name;
+    public:
+    frnd()
+    {
+        ch = 'H';
+        name = "Hemesh";
+        cout<<"Constructor"<<endl;
+    }
+
+    ~frnd()
+    {
+        cout<<"Destructor"<<endl;
+    }
+    void display()
+    {
+        cout<<"Name = "<<name<<endl;
+        cout<<"Starting character of name = "<<ch<<endl;
+    }
+
+    friend void create_obj();
+};
+
+void create_obj()
+{
+    frnd f;
+    f.display();
+}
+
+int main()
+{
+    create_obj();
+
+    return 0;
+
+}
