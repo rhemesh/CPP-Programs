0568e2c7c6cfea09195acd40562cd59c0f3adeb5
rhemesh <hemeshr07@gmail.com>
2024-11-13 08:32:32 +0530

    usage of friend class it acts as a pass by reference

---
+++ friend_class.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    int a;
+    public:
+    A()
+    {
+        a = 10;
+        cout<<"Constructor"<<endl;
+    }
+    ~A()
+    {
+        cout<<"Destructor"<<endl;
+    }
+    void shw()
+    {
+        cout<<"a = "<<a<<endl;
+    }
+
+    friend class B;
+};
+
+class B
+{
+    int b;
+    public:
+    B()
+    {
+        b = 20;
+    }
+    void show(A& x)
+    {
+        x.a = 30;
+        cout<<"x.a  = "<<x.a<<endl;
+        cout<<"b = "<<b<<endl;
+    }
+};
+int main()
+{
+    A a;
+    B b;
+    a.shw();
+    b.show(a);
+    a.shw();
+
+    return 0;
+
+}
+
+//This acts as pass by reference in friend class because we are passing the obj refrence to the class B function
+
+//If we use friend class we can modify the content of the class A variables in class B.
