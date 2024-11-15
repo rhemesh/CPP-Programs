662d4ebb70872071bd01bce1c91d55348d509044
rhemesh <hemeshr07@gmail.com>
2024-11-15 14:03:56 +0530

    implementing list

---
+++ STL/Implementing_list.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+int main()
+{
+    list<int> l;
+    for(int i=0;i<10;i++)
+    {
+        l.push_back(i);
+    }
+    cout<<"Size of list  =  "<<l.size()<<endl;
+
+    list<int>::iterator lst = l.begin();
+
+    while(lst != l.end())
+    {
+        cout<<*lst<<endl;
+        lst++;
+    }
+
+}
