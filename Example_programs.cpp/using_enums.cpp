f85668912b9ccdd7afe11d12447c6900f7c9705f
rhemesh <hemeshr07@gmail.com>
2024-11-19 15:48:08 +0530

    Enums

---
+++ Example_programs.cpp/using_enums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+enum months {january = 1,february,march,april,may,june,july,august,september,october,november,december};
+
+int main()
+{
+    months m;
+    m = june;
+    cout<<"Month of birthday = "<<june;
+    cout<<march;
+}
