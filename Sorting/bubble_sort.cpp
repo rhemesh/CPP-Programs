b0b7523f9e367e47e58232003e04bb15bd5b06e0
rhemesh <hemeshr07@gmail.com>
2024-11-19 15:53:45 +0530

    .\bubble_sort.cpp

---
+++ Sorting/bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter num value:"<<endl;
+    cin>>num;
+    int arr[num];
+    cout<<"Enter the array elements:"<<endl;
+    for(int i=0;i<num;i++)
+    {
+        cin>>arr[i];
+    }
+    int temp = 0;
+    for(int i=0;i<num;i++)
+    {
+        for(int j=i+1;j<num;j++)
+        {
+            if(arr[j] < arr[i])
+            {
+                arr[i] = arr[i]^arr[j];
+                arr[j] = arr[i]^arr[j];
+                arr[i] = arr[i]^arr[j];
+            }
+        }
+    }
+
+    cout<<"After bubble sort array elements are:"<<endl;
+    for(int i=0;i<num;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+
+}
